166f80207ee48b16ce6851f95e9410add45e5201
Kinorsi <kinorsi@gmail.com>
2015-11-13 17:31:14 +0800

    登陆

--- dxc/app/api/login.py
+++ dxc/app/api/login.py
@@ -1,6 +1,8 @@
 #-*- coding:utf-8 -*-
 
-from flask import Blueprint, url_for, current_app
+from . import route
+
+from flask import Blueprint, url_for, current_app, request
 import requests
 import re
 import json
@@ -10,16 +12,21 @@ from . import jsonres
 bp = Blueprint('api_login', __name__)
 
 
-@bp.route('/login', methods=['GET'])
+@bp.route('/login', methods=['POST'])
 def login():
     """"""
+
+    username = request.json.get('username', '')
+    passwd = request.json.get('password', '')
+
+    # print username
 
     loginUrl = url_for('security.login')
     loginUrl = current_app.config['API_URL'] + loginUrl
 
     res = requests.get(loginUrl)
 
-    print 'cookies:', res.headers['set-cookie']
+    # print 'cookies:', res.headers['set-cookie']
     content = res.text
     cookies = res.cookies #Must send back the cookies, or the server can't make correct crsf_token
 
@@ -27,12 +34,17 @@ def login():
 
     match = re.search(pattern, content)
     csrf_token = match.group(1)
-    print 'csrf_token:', csrf_token
+    # print 'csrf_token:', csrf_token
 
-    data = {'csrf_token':csrf_token,'email':'172440249@qq.com', 'password':'luyasi'}
+    data = {'csrf_token':csrf_token,'email':username, 'password': passwd}
     jdata = json.dumps(data)
     loginRes = requests.post(loginUrl, json=data, cookies=cookies)
     return loginRes.text
     #print loginRes
 
     # return jsonres(rv=dict(msg='ok'))
+
+
+@route(bp, '/testlogin', methods=['GET'])
+def testlogin():
+    return 'ok'
